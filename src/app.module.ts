a6bdbe19ff2c83228e71c062a980fffc31604b37
John Marvin Doniña <jbmarvin24@gmail.com>
2023-06-17 07:26:37 +0800

    add controller examples

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventsController } from './events.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'password',
+      database: 'nest-events',
+    }),
+  ],
   controllers: [AppController, EventsController],
   providers: [AppService],
 })
--- src/events.controller.ts
+++ src/events.controller.ts
@@ -1,19 +1,65 @@
-import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CreateEventDto } from './create-event.dto';
+import { Event } from './event.entity';
+import { UpdateEventDto } from './update-event.dto';
 
 @Controller('/events')
 export class EventsController {
-  @Get()
-  findAll() {}
+  private events: Event[] = [];
 
   @Get()
-  findOne() {}
+  findAll() {
+    return this.events;
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id) {
+    const event = this.events.find((e) => e.id === parseInt(id));
+
+    return event;
+  }
 
   @Post()
-  create() {}
+  create(@Body() dto: CreateEventDto) {
+    const createdEvent: Event = {
+      ...dto,
+      when: new Date(dto.when),
+      id: this.events.length + 1,
+    };
 
-  @Patch()
-  update() {}
+    this.events.push(createdEvent);
 
-  @Delete()
-  remove() {}
+    return createdEvent;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id, @Body() dto: UpdateEventDto) {
+    const index = this.events.findIndex((e) => e.id === parseInt(id));
+
+    const updatedEvent = {
+      ...this.events[index],
+      ...dto,
+      when: dto.when ? new Date(dto.when) : this.events[index].when,
+    };
+
+    this.events[index] = updatedEvent;
+
+    return updatedEvent;
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  remove(@Param('id') id) {
+    this.events = this.events.filter((e) => e.id !== parseInt(id));
+  }
 }
