5175ff33258b0c2f6a7c5938c05774b751134419
John Marvin Doniña <jbmarvin24@gmail.com>
2023-06-18 09:05:01 +0800

    Implement JWT Login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,17 @@
 import { Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
   @Post('login')
   @UseGuards(AuthGuard('local'))
   async login(@Req() request) {
     return {
       userId: request.user.id,
-      token: 'token here',
+      token: this.authService.getTokenForUser(request.user),
     };
   }
 }
