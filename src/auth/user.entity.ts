aa0ff3c928d3ba50f90ba1d617cc558ce8cb0e76
John Marvin Doniña <jbmarvin24@gmail.com>
2023-06-18 08:10:28 +0800

    Add user and profile entity

---
+++ src/auth/user.entity.ts
@@ -0,0 +1,30 @@
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Profile } from './profile.entity';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  firstName: string;
+
+  @OneToOne(() => Profile)
+  @JoinColumn()
+  profile: Profile;
+}
--- src/config/orm.config.prod.ts
+++ src/config/orm.config.prod.ts
@@ -2,6 +2,10 @@ import { registerAs } from '@nestjs/config';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { Event } from './../events/event.entity';
 import { Attendee } from 'src/events/attendee.entity';
+import { Subject } from 'rxjs';
+import { Profile } from '../auth/profile.entity';
+import { User } from '../auth/user.entity';
+import { Teacher } from '../school/teacher.entity';
 
 export default registerAs(
   'orm.config',
@@ -12,7 +16,7 @@ export default registerAs(
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    entities: [Event, Attendee],
+    entities: [Event, Attendee, Subject, Teacher, User, Profile],
     synchronize: false,
   }),
 );
