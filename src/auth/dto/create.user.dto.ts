3e0211bb63275873eb8b62622cfa289e39ae7f9e
John Marvin Doniña <jbmarvin24@gmail.com>
2023-06-29 16:47:52 +0800

    Add User Add mutation

--- src/auth/dto/create.user.dto.ts
+++ src/auth/dto/create.user.dto.ts
@@ -1,23 +1,30 @@
+import { Field, InputType } from '@nestjs/graphql';
 import { IsEmail, Length } from 'class-validator';
 import { IsRepeated } from '../../validation/is-repeated.constraint';
 import { UserDoesNotExist } from '../validation/user-does-not-exist.constraint';
 
+@InputType('UserAddInput')
 export class CreateUserDto {
   @Length(5)
   @UserDoesNotExist()
+  @Field()
   username: string;
 
   @Length(8)
+  @Field()
   password: string;
 
   @Length(8)
   @IsRepeated('password')
+  @Field()
   retypedPassword: string;
 
   @Length(2)
+  @Field()
   firstName: string;
 
   @IsEmail()
   @UserDoesNotExist()
+  @Field()
   email: string;
 }
--- src/auth/user.resolver.ts
+++ src/auth/user.resolver.ts
@@ -1,14 +1,23 @@
 import { UseGuards } from '@nestjs/common';
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { AuthGuardJwtGql } from './auth-guard-jwt.gql';
 import { CurrentUser } from './current-user.decorator';
+import { CreateUserDto } from './dto/create.user.dto';
 import { User } from './user.entity';
+import { UserService } from './user.service';
 
 @Resolver(() => User)
 export class UserResolver {
+  constructor(private readonly userService: UserService) {}
+
   @Query(() => User, { nullable: true })
   @UseGuards(AuthGuardJwtGql)
   public async me(@CurrentUser() user: User): Promise<User> {
     return user;
+  }
+
+  @Mutation(() => User, { name: 'userAdd' })
+  public async add(@Args('input') input: CreateUserDto): Promise<User> {
+    return await this.userService.create(input);
   }
 }
